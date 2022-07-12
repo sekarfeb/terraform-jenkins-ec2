7d9b7dfc62b0f4f1887e7ef1cde9eae3e0b3a357
sekarfeb <sekarfeb@gmail.com>
2022-07-12 16:25:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,15 @@
 provider "aws" {
-  region = "${var.region}"
-  profile = "prashant_appgambit"
+  access_key = "AKIA5SXAAVYWETRT2ZCC"
+  secret_key = "jL8X6Xunl9BhWOwiRR+j54KxDISGX7Gyf4YiA0zd"
+  region     = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0341aeea105412b57"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "sekar-img"
+  }
+
 }
